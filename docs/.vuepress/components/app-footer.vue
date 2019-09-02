28dd62b54583c60c43eb7e63969ca424aa7bffed
Anand Graves <anand@voorhoede.nl>
2019-09-02 21:02:19 +0200

    feat: add footer with credits after content

---
+++ docs/.vuepress/components/app-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-footer">Digital Design Dictionary by <a href="https://hike.one"><img src="/logo.svg" alt="Logo of Hike One" height="15" /></a></div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .app-footer {
+    color: #7c7c7c;
+  }
+</style>
