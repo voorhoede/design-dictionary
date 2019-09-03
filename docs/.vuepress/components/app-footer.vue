f35aefa28731d5b47adb05fb0bd057192a2d1963
Anand Graves <anand@voorhoede.nl>
2019-09-03 13:59:19 +0200

    refactor: use a bg color

--- docs/.vuepress/components/app-footer.vue
+++ docs/.vuepress/components/app-footer.vue
@@ -10,28 +10,22 @@ export default {
 <style>
   .app-footer {
     position: fixed;
-    width: 305px;
-    bottom: 10px;
+    width: 100%;
+    bottom: 0;
     z-index: 1;
     left: 50%;
+    padding: 10px 0;
     transform: translate(-50%, 0);
+    text-align: center;
     color: #585858;
-    background-color: white;
+    background-color: #f8f8f8;
   }
 
   @media (min-width: 720px) {
     .app-footer {
-      width: auto;
-      left: 370px;
+      left: 0;
       transform: none;
-    }
-  }
-
-
-  @media (min-width: 900px) {
-    .app-footer {
-      left: 50%;
-      transform: none;
+      padding-left: 340px;
     }
   }
 </style>
